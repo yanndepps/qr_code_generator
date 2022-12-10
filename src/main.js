32f2acf585fc09df6070d4ead10bea9ccc224425
yanndepps <yann.deperrot@protonmail.com>
2022-12-10 11:04:50 +0100

    done generator

--- src/main.js
+++ src/main.js
@@ -19,7 +19,12 @@ const onGenerateSubmit = (e) => {
     setTimeout(() => {
       hideSpinner();
       generateQRCode(url, size);
-    }, 3000);
+
+      setTimeout(() => {
+        const saveUrl = qr.querySelector('img').src;
+        createSaveBtn(saveUrl);
+      }, 50);
+    }, 1000);
   }
 };
 
@@ -41,11 +46,19 @@ const hideSpinner = () => {
 
 const clearUI = () => {
   qr.innerHTML = '';
+  const saveLink = document.getElementById('save-link');
+  if (saveLink) saveLink.remove();
 };
 
 const createSaveBtn = (saveUrl) => {
-  //---
-}
+  const link = document.createElement('a');
+  link.id = 'save-link';
+  link.classList = 'bg-red-500 hover:bg-red-700 text-white font-bold py-2 rounded w-1/3 m-auto my-5';
+  link.href = saveUrl;
+  link.download = 'qrcode';
+  link.innerHTML = 'Save Image';
+  document.getElementById('generated').appendChild(link);
+};
 
 hideSpinner();
 
